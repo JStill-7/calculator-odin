73cf7059381b4f91c23e15ee0d27afdd6ad6fb0d
Joshua Still <Joshua_Still@outlook.com>
2025-04-23 16:00:53 -0600

    added basic functions

---
+++ app.js
@@ -0,0 +1,24 @@
+let num1
+let num2
+let operator
+
+
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function operate(num1, num2, operator) {
+    return num1 + operator + num2;
+}

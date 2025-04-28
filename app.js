a959b9f01938f3e26921433395a7bfc213b1d6c0
Joshua Still <Joshua_Still@outlook.com>
2025-04-28 15:28:37 -0600

    added html and basic css to calculator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let num1
 let num2
 let operator
+let equalsBtn = document.querySelector('.equals');
 
 
 function add(a,b) {
@@ -22,3 +23,7 @@ function divide(a,b) {
 function operate(num1, num2, operator) {
     return num1 + operator + num2;
 }
+
+equalsBtn.addEventListener('click', (e) => {
+    console.log('testing result')
+})
